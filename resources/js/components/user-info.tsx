3da74a4b7c7aa1fd19996bf66317c62d41e840e4
eddysantiagoo <eddysantiagogh@gmail.com>
2025-03-12 14:22:56 -0500

    asasasasas

--- resources/js/components/user-info.tsx
+++ resources/js/components/user-info.tsx
@@ -2,7 +2,7 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { useInitials } from '@/hooks/use-initials';
 import { type User } from '@/types';
 
-export function UserInfo({ user, showEmail = false }: { user: User; showEmail?: boolean }) {
+export function UserInfo () {
     const getInitials = useInitials();
 
     return (
