510728fb7ccd3822b7b2dcd10332d6f9f1a4a22b
eddysantiagoo <eddysantiagogh@gmail.com>
2025-03-12 13:44:01 -0500

    asasasa

--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,13 @@ Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
-    Route::get('pokemons', function () {
-        return Inertia::render('todos/page');
-    })->name('pok');
-});
+Route::get('dashboard', function () {
+    return Inertia::render('dashboard');
+})->name('dashboard');
+
+Route::get('pokemons', function () {
+    return Inertia::render('todos/page');
+})->name('pok');
 
 require __DIR__ . '/settings.php';
 require __DIR__ . '/auth.php';
