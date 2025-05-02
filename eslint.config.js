0fff5c656af4856b03b26fc4ac75ad0885e077ac
eddysantiagoo <eddysantiagogh@gmail.com>
2025-05-02 02:34:49 -0500

    fix: disable specific TypeScript and React hooks rules in ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -21,6 +21,10 @@ export default [
             'react/react-in-jsx-scope': 'off',
             'react/prop-types': 'off',
             'react/no-unescaped-entities': 'off',
+            '@typescript-eslint/no-unused-vars': 'off',
+            '@typescript-eslint/no-explicit-any': 'off',
+            '@typescript-eslint/no-non-null-asserted-optional-chain': 'off',
+            'react-hooks/exhaustive-deps': 'off',
         },
         settings: {
             react: {
@@ -34,7 +38,6 @@ export default [
         },
         rules: {
             'react-hooks/rules-of-hooks': 'error',
-            'react-hooks/exhaustive-deps': 'warn',
         },
     },
     {
